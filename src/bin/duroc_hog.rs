53115b5d3a482ab22eff58999a5c61cc21f2e599
Scott Cutler <scutler@newrelic.com>
2020-03-25 16:02:49 -0700

    working commit, scans 1 level deep for ZIPs

--- src/bin/duroc_hog.rs
+++ src/bin/duroc_hog.rs
@@ -60,6 +60,8 @@ pub struct FileFinding {
     pub linenum: usize
 }
 
+const ZIPEXTENSIONS: &'static [&'static str] = &["zip"];
+
 /// Main entry function that uses the [clap crate](https://docs.rs/clap/2.33.0/clap/)
 fn main() {
     let matches = clap_app!(choctaw_hog =>
@@ -102,75 +104,113 @@ fn run(arg_matches: &ArgMatches) -> Result<(), SimpleError> {
         info!("path verification succeeded");
     }
 
+    let mut output: HashSet<FileFinding> = HashSet::new();
+
     if Path::is_dir(fspath) {
-        if recursive {
-            for entry in WalkDir::new(fspath).into_iter().filter_map(|e| e.ok()) {
-                let inner_findings = scan_file(entry.path(), &secret_scanner);
-                info!("inner findings: {:?}", inner_findings);
-            }
-        } else {
-            let dir_contents: Vec<PathBuf> = fspath.read_dir()
-                .expect("read_dir call failed")
-                .filter_map(|e| e.ok())
-                .filter(|e| e.file_type().unwrap().is_file())
-                .map(|e| e.path())
-                .collect();
-            info!("dir_contents: {:?}", dir_contents);
-            for file_path in dir_contents {
-                let inner_findings = scan_file(file_path.as_path(), &secret_scanner);
-                info!("inner findings: {:?}", inner_findings);
+        output.extend(scan_dir(fspath, &secret_scanner, recursive));
+    } else {
+        output.extend(scan_file(fspath, &secret_scanner));
+    }
+
+    info!("Found {} secrets", output.len());
+    secret_scanner.output_findings(&output);
+
+    Ok(())
+}
+
+fn scan_dir(fspath: &Path, ss: &SecretScanner, recursive: bool) -> HashSet<FileFinding> {
+    let mut output: HashSet<FileFinding> = HashSet::new();
+    if recursive {
+        for entry in WalkDir::new(fspath).into_iter().filter_map(|e| e.ok()) {
+            if entry.file_type().is_file() {
+                let mut inner_findings = scan_file(entry.path(), &ss);
+                for d in inner_findings.drain() {
+                    output.insert(d);
+                }
             }
         }
     } else {
-        let inner_findings = scan_file(fspath, &secret_scanner);
-        info!("inner findings: {:?}", inner_findings);
-    }
-
-    fn scan_file(file_path: &Path, ss: &SecretScanner) -> HashSet<FileFinding> {
-        let mut findings: HashSet<FileFinding> = HashSet::new();
-        info!("scan_file({:?})", file_path);
-        let ext: String = match file_path.extension() {
-            Some(osstr) => String::from(osstr.to_str().unwrap_or_else(|| "")),
-            None => String::from("")
-        };
-
-        if file_path.to_str().unwrap() == "./output.json" {
-            debug!("output.json")
+        let dir_contents: Vec<PathBuf> = fspath.read_dir()
+            .expect("read_dir call failed")
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().unwrap().is_file())
+            .map(|e| e.path())
+            .collect();
+        // info!("dir_contents: {:?}", dir_contents);
+        for file_path in dir_contents {
+            let mut inner_findings = scan_file(file_path.as_path(), &ss);
+            for d in inner_findings.drain() {
+                info!("FileFinding: {:?}",d);
+                output.insert(d);
+            }
+            // info!("inner findings: {:?}", inner_findings);
         }
-        let mut f = File::open(file_path).unwrap();
-        let mut data = Vec::new();
-        f.read_to_end(&mut data);
+    }
+    output
+}
 
-        // Main loop - split the data based on newlines, then run get_matches() on each line,
-        // then make a list of findings in output
-        let lines = data.split(|&x| (x as char) == '\n');
-        for (index, new_line) in lines.enumerate() {
-            let results = ss.matches(new_line);
-            for (r, matches) in results {
-                let mut strings_found: Vec<String> = Vec::new();
-                for m in matches {
-                    let result = ASCII
-                        .decode(&new_line[m.start()..m.end()], DecoderTrap::Ignore)
-                        .unwrap_or_else(|_| "<STRING DECODE ERROR>".parse().unwrap());
-                    strings_found.push(result);
-                }
-                if !strings_found.is_empty() {
-                    let new_line_string = ASCII
-                        .decode(&new_line, DecoderTrap::Ignore)
-                        .unwrap_or_else(|_| "<STRING DECODE ERROR>".parse().unwrap());
-                    findings.insert(FileFinding {
-                        strings_found,
-                        reason: r.clone(),
-                        path: String::from(file_path.to_str().unwrap()),
-                        linenum: index + 1
-                    });
-                }
+fn scan_file(file_path: &Path, ss: &SecretScanner) -> HashSet<FileFinding> {
+    let mut findings: HashSet<FileFinding> = HashSet::new();
+    info!("scan_file({:?})", file_path);
+    let ext: String = match file_path.extension() {
+        Some(osstr) => String::from(osstr.to_str().unwrap_or_else(|| "")).to_ascii_lowercase(),
+        None => String::from("")
+    };
+
+    let mut f = File::open(file_path).unwrap();
+
+    // https://stackoverflow.com/questions/23975391/how-to-convert-a-string-into-a-static-str
+    return if ZIPEXTENSIONS.contains(&&*ext) {
+        let mut zip = zip::ZipArchive::new(f).unwrap();
+        for i in 0..zip.len()
+        {
+            let mut innerfile = zip.by_index(i).unwrap();
+            let mut data = Vec::new();
+            innerfile.read_to_end(&mut data);
+            let inner_path = format!("{}/{}", file_path.display(), innerfile.name());
+            info!("Scanning inner_path: {}", inner_path);
+            let mut inner_findings = scan_bytes(data, ss, inner_path);
+            for d in inner_findings.drain() {
+                info!("FileFinding: {:?}",d);
+                findings.insert(d);
             }
         }
-
-
         findings
+    } else {
+        let mut data = Vec::new();
+        f.read_to_end(&mut data);
+        scan_bytes(data, ss, String::from(file_path.to_str().unwrap()))
     }
 
-    Ok(())
+}
+
+fn scan_bytes(input: Vec<u8>, ss: &SecretScanner, path: String) -> HashSet<FileFinding> {
+    let mut findings: HashSet<FileFinding> = HashSet::new();
+    // Main loop - split the data based on newlines, then run get_matches() on each line,
+    // then make a list of findings in output
+    let lines = input.split(|&x| (x as char) == '\n');
+    for (index, new_line) in lines.enumerate() {
+        let results = ss.matches(new_line);
+        for (r, matches) in results {
+            let mut strings_found: Vec<String> = Vec::new();
+            for m in matches {
+                let result = ASCII
+                    .decode(&new_line[m.start()..m.end()], DecoderTrap::Ignore)
+                    .unwrap_or_else(|_| "<STRING DECODE ERROR>".parse().unwrap());
+                strings_found.push(result);
+            }
+            if !strings_found.is_empty() {
+                let new_line_string = ASCII
+                    .decode(&new_line, DecoderTrap::Ignore)
+                    .unwrap_or_else(|_| "<STRING DECODE ERROR>".parse().unwrap());
+                findings.insert(FileFinding {
+                    strings_found,
+                    reason: r.clone(),
+                    path: path.clone(),
+                    linenum: index + 1
+                });
+            }
+        }
+    }
+    findings
 }
